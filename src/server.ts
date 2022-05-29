a0bbcd59c48dfa469332f8c4558fc7d3cb372595
HallexCosta <hallex.costa@hotmail.com>
2022-05-28 22:52:43 -0300

    chore: add default port 3333 from app

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import http from 'node:http'
 import { Pool } from 'pg'
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3333
 const PG_URL = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 console.log('postgre_url', PG_URL)
 
