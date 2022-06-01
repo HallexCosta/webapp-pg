33cae3d2e28b729d44a66cb2be8d2dbe4520dad1
HallexCosta <hallex.costa@hotmail.com>
2022-06-01 14:27:09 -0300

    chore: start with cluster

--- requests.sh
+++ requests.sh
@@ -1,8 +1,13 @@
- curl -X POST http://localhost:3333/db-login \
+URL_PROD=http://webapp-pg.herokuapp.com
+URL_DEV=http://localhost:3333
+URL=$URL_PROD
+
+echo "First request"
+ curl -X POST $URL/db-login \
       -d '{"name": "naanzjml", "host": "drona.db.elephantsql.com", "user": "naanzjml", "password": "0Da9q1BwphDg0hjV5vfEcqTeWTzlV_HI", "port": 5432}'
 
 echo "\n\nSecond request"
-curl -X POST http://localhost:3333/pg \
+curl -X POST $URL/pg \
       -d '{"query": "select * from animes"}' \
-      -H "Content-Type:: application/json" \
+      -H "Content-Type: application/json" \
       -H "token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJob3N0IjoiZHJvbmEuZGIuZWxlcGhhbnRzcWwuY29tIiwibmFtZSI6Im5hYW56am1sIiwidXNlciI6Im5hYW56am1sIiwicGFzc3dvcmQiOiIwRGE5cTFCd3BoRGcwaGpWNXZmRWNxVGVXVHpsVl9ISSIsInBvcnQiOjU0MzIsImlhdCI6MTY1NDAyMzQ4Nywic3ViIjoibmFhbnpqbWwifQ.5Hz2Ajr5ahJZHfmrsLCca-dXjxpBZCDRMqWA-VKEw8c"
